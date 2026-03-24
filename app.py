fe187846041ad7900c296580757244f2845eb8d7
andrelo1427-cell <andrelo.1427@gmail.com>
2026-03-24 18:31:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,5 @@ def actualizar_grafico(cilindro):
 # ======================
 # RUN
 # ======================
-if __name__ == '__main__':
-    app.run(debug=True)
-
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=8050)
