c86d3b9d42d5f61f311e8e031985cef219c6097a
andrelo1427-cell <andrelo.1427@gmail.com>
2026-03-24 18:10:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -142,3 +142,6 @@ def actualizar_grafico(cilindro):
 # ======================
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=8050)
